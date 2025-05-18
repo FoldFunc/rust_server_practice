31c3ed3f6805696d8ec77c53d39bc01b4969e011
FoldFunc <fold.func@gmail.com>
2025-05-18 19:03:13 +0200

    Start of a function that will be responsible for calling a server endpoint that will update stocks price

---
+++ middlewear_for_updating_price/src/main.rs
@@ -0,0 +1,27 @@
+use reqwest::Client;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct ToSent {
+    name: String,
+    secret_key: String,
+}
+// Make a function that will fetch all of the crypto names and update the "name" in the secret_key so the call to the server endpoint will be possibe
+// Also need to change server endpoint to accept the "secret" key not a cookie.
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let secret_key = ToSent {
+        name: "name".to_string(),
+        secret_key: "secret_no_tell".to_string(),
+    };
+    loop {
+        let res = client
+            .post("http://localhost:8080/api/root/changeprice")
+            .json(&secret_key)
+            .send()
+            .await?;
+        println!("Status: {}", res.status());
+        println!("Response: {}", res.text().await?);
+    }
+}
